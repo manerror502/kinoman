9a31debc35cf34cea504e6d5399988e9de610636
Man--net <manylov12@gmail.com>
2020-07-31 00:37:06 +0500

    Добавлен

---
+++ src/utils/messages.js
@@ -0,0 +1,8 @@
+/* eslint-disable quote-props */
+export default {
+  'logout': 'Вы вышли из системы',
+  'auth/user-not-found': 'Пользователь с таким E-mail не найден',
+  'auth/wrong-password': 'Неверный пароль',
+  'auth/email-already-in-use': 'Пользователь с таким E-mail уже существует',
+  'auth/too-many-requests': 'Слишком много неудачных попыток входа в систему. Пожалуйста, попробуйте позже'
+}
