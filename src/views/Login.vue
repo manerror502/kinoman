f431a6c956e59506da11ce3e70a30cf4c7689fad
Man--net <manylov12@gmail.com>
2020-07-31 00:35:43 +0500

    Добавлена валидация

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,119 +1,104 @@
 <template>
   <form
     class="sign__login"
+    @submit.prevent="submitHandler"
   >
     <h2>Войти в аккаунт КиноMan</h2>
-    <small>Поле E-mail не должно быть пустым</small>
-    <small>Введите коректный E-mail</small>
-    <input
-      autofocus
-      type="text"
-      placeholder="Электронная почта"
-    >
-    <small>Введите пароль</small>
-    <input
-      type="password"
-      placeholder="Пароль"
-    >
 
-    <button
-      class="btn btn--login"
-      type="submit"
-    >
-      Войти
-    </button>
+    <div class="sign__wrap">
+      <small
+        v-if="$v.email.$dirty && !$v.email.required"
+      >Поле E-mail не должно быть пустым</small>
+      <small
+        v-else-if="$v.email.$dirty && !$v.email.email"
+      >Введите коректный E-mail</small>
+      <input
+        autofocus
+        type="text"
+        placeholder="Электронная почта"
+        v-model.trim="email"
+        :class="{invalid: ($v.email.$dirty && !$v.email.required) || ($v.email.$dirty && !$v.email.email)}"
+      >
+      <small
+        v-if="$v.password.$dirty && !$v.password.required"
+      >Введите пароль</small>
+      <input
+        type="password"
+        placeholder="Пароль"
+        v-model.trim="password"
+        :class="{invalid: $v.password.$dirty && !$v.password.required}"
+      >
 
-    <router-link
-      tag="a"
-      to="/registered"
-    >
-      У меня ещё нет аккаунт КиноMan
-    </router-link>
+      <button
+        class="btn btn--login"
+        type="submit"
+      >
+        Войти
+      </button>
+
+      <router-link
+        tag="a"
+        to="/registered"
+      >
+        У меня ещё нет аккаунт КиноMan
+      </router-link>
+    </div>
   </form>
 </template>
 
 <script>
+import { email, required } from 'vuelidate/lib/validators'
+import messages from '@/utils/messages'
+
 export default {
-  name: 'Login'
+  name: 'Login',
+  data: () => ({
+    email: '',
+    password: ''
+  }),
+  validations: {
+    email: { email, required },
+    password: { required }
+  },
+
+  mounted () {
+    if (messages[this.$route.query.message]) {
+      this.$toast.open({
+        message: messages[this.$route.query.message],
+        type: 'info',
+        duration: 5000,
+        dismissible: true
+      })
+    }
+  },
+  methods: {
+    submitHandler () {
+      if (this.$v.$invalid) {
+        this.$v.$touch()
+        return
+      }
+      const formData = {
+        email: this.email,
+        password: this.password
+      }
+
+      console.log(formData)
+
+      this.$router.push('/')
+    }
+  }
 }
 </script>
 
 <style lang="less" >
 @import "@/assets/style/vars/vars.module";
 
-.sign__login {
-  text-align: center;
-  position: relative;
-
-  h2 {
-    font-family: @font-family__sans;
-    color: @colors__grays;
-    font-size: @font-size--normal + 5;
-    line-height: @line-height--large;
-    color: @colors__grays--lighter;
-    text-align: center;
-    margin-bottom: 20px;
-    box-shadow: @shadows__coords-x @shadows__coords-y @shadows__size
-      fade(@colors__black, 20%);
-  }
-
-  small {
-    display: none;
-    color: @colors__red;
-    font-size: @font-size--normal;
-    transition: @transition-duration @transition-timing-function;
-
-    &.invalid {
-      display: block;
-    }
-  }
-
-  input {
-    font-family: @font-family__sans;
-    font-size: @font-size--normal;
-    width: 250px;
-    border: 0;
-    background: none;
-    display: block;
-    margin: 20px auto;
-    margin-top: 10px;
-    text-align: center;
-    border: @border-width + 1 solid @colors__primary;
-    padding: 14px 10px;
-    outline: none;
-    color: @colors__white;
-    border-radius: @border-radius__large;
-    transition: @transition-duration @transition-timing-function;
-
-    &:focus {
-      width: 280px;
-      border-color: @colors__green;
-    }
-
-    &.invalid {
-      border: @border-width + 1 solid @colors__red;
-    }
-  }
-
-  a {
-    transition: @transition-duration @transition-timing-function;
-    color: @colors__grays;
-    text-decoration: underline;
-
-    &:hover,
-    &:focus {
-      color: @colors__green;
-      outline: none;
-    }
-  }
-}
-
 .btn--login {
   font-family: @font-family__sans;
   font-size: @font-size--normal - 5;
   color: @colors__grays--lighter;
   display: block;
-  margin: 20px auto;
+  margin: 20px 0;
   text-align: center;
   border: @border-width + 1 solid @colors__green;
   padding: 14px 70px;
--- src/views/Registered.vue
+++ src/views/Registered.vue
@@ -2,143 +2,110 @@
   <!--sign window registr-->
   <form
     class="sign__login"
+    @submit.prevent="onSubmit"
   >
     <h2>Создать аккаунт КиноMan</h2>
 
-    <small>Поле Имя не должно быть пустым</small>
-    <p>Ваше имя?</p>
-    <input
-      v-model.trim="userName"
-      autofocus
-      type="text"
-      placeholder="Имя пользователя"
-    >
+    <div class="sign__wrap">
+      <small v-if="$v.userName.$dirty && !$v.userName.required">Поле Имя не должно быть пустым</small>
+      <input
+        v-model.trim="userName"
+        autofocus
+        type="text"
+        placeholder="Имя пользователя"
+        :class="{ invalid: $v.userName.$dirty && !$v.userName.required }"
+      >
 
-    <small>Поле E-mail не должно быть пустым</small>
-    <small>Введите коректный E-mail</small>
+      <small v-if="$v.email.$dirty && !$v.email.required">Поле E-mail не должно быть пустым</small>
+      <small v-else-if="$v.email.$dirty && !$v.email.email">Введите коректный E-mail</small>
+      <input
+        v-model.trim="email"
+        type="text"
+        placeholder="Электонная почта"
+        :class="{
+          invalid:
+            ($v.email.$dirty && !$v.email.required) ||
+            ($v.email.$dirty && !$v.email.email),
+        }"
+      >
 
-    <p>Ваша Электонная почта?</p>
-    <input
-      type="text"
-      placeholder="Электонная почта"
-    >
+      <small
+        v-if="$v.password.$dirty && !$v.password.required"
+      >Введите пароль</small>
+      <small
+        v-else-if="$v.password.$dirty && !$v.password.minLength"
+      >Пароль должен быть {{ $v.password.$params.minLength.min }} символов
+        сейчас он {{ password.length }}</small>
 
-    <small>Возраст должен быть от до </small>
+      <input
+        v-model.trim="password"
+        type="password"
+        placeholder="Пароль"
+        :class="{
+          invalid:
+            ($v.password.$dirty && !$v.password.required) ||
+            ($v.password.$dirty && !$v.password.minLength),
+        }"
+      >
 
-    <p>Сколько вам лет?</p>
-    <input
-      type="text"
-      placeholder="Возраст"
-    >
+      <button
+        class=" btn--login  btn"
+        type="submit"
+      >
+        Зарегистрироваться
+      </button>
 
-    <small>Введите пароль</small>
-    <small>Пароль должен быть  символов
-      сейчас он </small>
-
-    <p>Ваш пароль? Я никому не расскажу!</p>
-    <input
-      type="password"
-      placeholder="Password"
-    >
-
-    <button
-      class="btn btn--login"
-      type="submit"
-    >
-      Зарегистрироваться
-    </button>
-
-    <router-link
-      tag="a"
-      to="/login"
-    >
-      У меня уже есть аккаунт КиноMan
-    </router-link>
+      <router-link
+        tag="a"
+        to="/login"
+      >
+        У меня уже есть аккаунт КиноMan
+      </router-link>
+    </div>
   </form>
 </template>
 
 <script>
+import { email, required, minLength } from 'vuelidate/lib/validators'
+
 export default {
-  name: 'Registr'
+  name: 'Registr',
+  data: () => ({
+    email: '',
+    password: '',
+    userName: ''
+  }),
+  validations: {
+    email: { email, required },
+    password: { required, minLength: minLength(8) },
+    userName: { required }
+  },
+  methods: {
+    async onSubmit () {
+      if (this.$v.$invalid) {
+        this.$v.$touch()
+        return
+      }
+      const formData = {
+        email: this.email,
+        password: this.password,
+        userName: this.userName
+      }
+      console.log(formData)
+    }
+  }
 }
 </script>
 
 <style lang="less" >
 @import "@/assets/style/vars/vars.module";
 
-.sign__login {
-  text-align: center;
-  position: relative;
-
-  h2 {
-    font-family: @font-family__sans;
-    color: @colors__grays;
-    font-size: @font-size--normal + 5;
-    line-height: @line-height--large;
-    color: @colors__grays--lighter;
-    text-align: center;
-    margin-bottom: 20px;
-    box-shadow: @shadows__coords-x @shadows__coords-y @shadows__size
-      fade(@colors__black, 20%);
-  }
-
-  small {
-    display: none;
-    color: @colors__red;
-    font-size: @font-size--normal;
-    transition: @transition-duration @transition-timing-function;
-
-    &.invalid {
-      display: block;
-    }
-  }
-
-  input {
-    font-family: @font-family__sans;
-    font-size: @font-size--normal;
-    width: 250px;
-    border: 0;
-    background: none;
-    display: block;
-    margin: 20px auto;
-    margin-top: 10px;
-    text-align: center;
-    border: @border-width + 1 solid @colors__primary;
-    padding: 14px 10px;
-    outline: none;
-    color: @colors__white;
-    border-radius: @border-radius__large;
-    transition: @transition-duration @transition-timing-function;
-
-    &:focus {
-      width: 280px;
-      border-color: @colors__green;
-    }
-
-    &.invalid {
-      border: @border-width + 1 solid @colors__red;
-    }
-  }
-
-  a {
-    transition: @transition-duration @transition-timing-function;
-    color: @colors__grays;
-    text-decoration: underline;
-
-    &:hover,
-    &:focus {
-      color: @colors__green;
-      outline: none;
-    }
-  }
-}
-
 .btn--login {
   font-family: @font-family__sans;
   font-size: @font-size--normal - 5;
   color: @colors__grays--lighter;
   display: block;
-  margin: 20px auto;
+  margin: 20px 0;
   text-align: center;
   border: @border-width + 1 solid @colors__green;
   padding: 14px 70px;
