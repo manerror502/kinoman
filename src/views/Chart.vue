c8d2bae36b06573d11b6e7c471ae84390c6e359c
ManError502 <59826140+manerror502@users.noreply.github.com>
2020-10-11 21:56:47 +0500

    Создана страница с чартом

---
+++ src/views/Chart.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="chart">
+    <div class="container-fluid">
+      <div class="row">
+        <div class="col">
+          <h3 class="chart__title">
+            Топ 1 в чарте
+          </h3>
+        </div>
+      </div>
+
+      <div class="row">
+        <div class="chart__top">
+          <div class="chart__top-film film">
+            <div class="film__img">
+              <img
+                src="https://kinopoiskapiunofficial.tech/images/posters/kp/1309166.jpg"
+                alt=""
+                class="img"
+              >
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
